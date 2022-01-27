139e30e0c98b109c5ca83d24ae36933fb848c59c
critiqalfish <69545904+critiqalfish@users.noreply.github.com>
2022-01-27 23:24:13 +0100

    master

--- static/JavaScript/plshelpme.js
+++ static/JavaScript/plshelpme.js
@@ -1,10 +1,9 @@
-function copyToClipBoard() {
+function copyToClipBoard(id) {
     var range = document.createRange();
-    range.selectNode(document.getElementById("a"));
+    range.selectNode(document.getElementById(id));
     window.getSelection().removeAllRanges(); // clear current selection
     window.getSelection().addRange(range); // to select text
     document.execCommand("copy");
     alert("Copied the text: " + range);
     window.getSelection().removeAllRanges();// to deselect
-  
 }
