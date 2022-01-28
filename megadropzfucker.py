a9b5ab755fa4f41de777db800f9b5c62bdabf402
critiqalfish <69545904+critiqalfish@users.noreply.github.com>
2022-01-28 13:15:35 +0100

    a

--- megadropzfucker.py
+++ megadropzfucker.py
@@ -1,4 +1,5 @@
 import requests
+import timeit
 
 class Error(Exception):
     pass
@@ -33,13 +34,18 @@ class OtherError(Error):
     def __str__(self):
         return ("OtherError")
 
-def link(link0):
+def link(link0, proxy=False):
     try: bypassed_link = requests.get(f'https://bypass.bot.nu/bypass2?url={link0}').json()
     except: raise RequestError()
     if not 'msg' in bypassed_link and bypassed_link['success']: pass
     elif not bypassed_link['success']: raise InvalidLinkError()
-    try: scraped_pastebin = requests.get(bypassed_link['destination'], proxies={'http': 'http://customer-5F4SU4D4GZ-cc-RU:164227LSSR@pr.proxy.onlinesim.ru:7777', 'https': 'http://customer-5F4SU4D4GZ-cc-RU:164227LSSR@pr.proxy.onlinesim.ru:7777'}).text
-    except: raise RequestError()
+    print(bypassed_link['destination'])
+    if proxy:
+        try: scraped_pastebin = requests.get(bypassed_link['destination'], proxies={'http': 'http://customer-5F4SU4D4GZ-cc-RU:164227LSSR@pr.proxy.onlinesim.ru:7777', 'https': 'http://customer-5F4SU4D4GZ-cc-RU:164227LSSR@pr.proxy.onlinesim.ru:7777'}).text
+        except: raise RequestError()
+    elif not proxy:
+        try: scraped_pastebin = requests.get(f"http://api.scraperapi.com?api_key=c665572ee042a836770340e6f54ab055&url={bypassed_link['destination']}").text
+        except: raise RequestError()
     try: extracted_link = scraped_pastebin[scraped_pastebin.index('<textarea class="textarea">')+len('<textarea class="textarea">'):scraped_pastebin.index('</textarea>')]
     except: raise LinkNotFoundError()
     try:
@@ -58,4 +64,4 @@ def link(link0):
     except: raise OtherError()
 
 if __name__ == '__main__':
-    print(link('https://direct-link.net/156589/84731'))
+    print('[AVERAGE]: ', 0.1 * int(timeit.timeit(lambda: print(link('https://link-center.net/156589/87347845', False)), number=10)))
--- test.py
+++ test.py
@@ -1,26 +1,4 @@
-import sqlite3
+import requests
 
-db = sqlite3.connect('database.db')
-cursor = db.cursor()
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS links (
-                ilink text,
-                olink text
-            )""")
-db.commit()
-cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS xlink ON links (olink)")
-db.commit()
-
-ilink = 'https://link-hub.net/156589/523488'
-
-with db:
-    cursor.execute("SELECT olink FROM links WHERE ilink=?", (ilink,))
-    dlinks = cursor.fetchall()
-
-if dlinks == []:
-    print('empty')
-else:
-    for link in dlinks:
-        print(link[0])
-
-db.close()
+r = requests.get(f"http://api.scraperapi.com?api_key=c665572ee042a836770340e6f54ab055&url=https://pastebin.com/hTyvZjYC").text
+print(r)
