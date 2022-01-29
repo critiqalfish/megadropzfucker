a71602506b4cbd993e692cd96a6f9683156f8a70
137-Trimethylxanthin <xxv.many@gmail.com>
2022-01-29 04:27:53 +0100

    Version 1.3 i FUKA JAVA AAAAAAAAAAAAA

--- static/JavaScript/plshelpme.js
+++ static/JavaScript/plshelpme.js
@@ -7,3 +7,13 @@ function copyToClipBoard(id) {
     alert("Copied the text: " + range);
     window.getSelection().removeAllRanges();// to deselect
 }
+function copy1(){
+    navigator.clipboard.writeText
+    ("Dextromethorphanhydrobromid#8794");
+    alert("Copied the Dicord-Tag: Dextromethorphanhydrobromid#8794" );
+}
+function copy2(){
+    navigator.clipboard.writeText
+    ("1,3,7-Trimethylxanthin#4309");
+    alert("Copied the Discord-Tag: Dextromethorphanhydrobromid#8794" );
+}
